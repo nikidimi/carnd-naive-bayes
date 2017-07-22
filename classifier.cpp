9ba9818732cf3161db7aaf72998906694ce13f0c
Nikolay Dimitrov <nikidimi@gmail.com>
2017-07-23 00:44:10 +0300

    Naive Bayes implementation

--- classifier.cpp
+++ classifier.cpp
@@ -16,44 +16,104 @@ GNB::~GNB() {}
 
 void GNB::train(vector<vector<double>> data, vector<string> labels)
 {
+  for(int i = 0; i < data.size(); i++) {
+    if (labels[i] == "left") {
+      left.add_data(data[i]);
+    }
+    if (labels[i] == "keep") {
+      keep.add_data(data[i]);
+    }
+    if (labels[i] == "right") {
+      right.add_data(data[i]);
+    }
+  }
 
-	/*
-		Trains the classifier with N data points and labels.
-
-		INPUTS
-		data - array of N observations
-		  - Each observation is a tuple with 4 values: s, d,
-		    s_dot and d_dot.
-		  - Example : [
-			  	[3.5, 0.1, 5.9, -0.02],
-			  	[8.0, -0.3, 3.0, 2.2],
-			  	...
-		  	]
-
-		labels - array of N labels
-		  - Each label is one of "left", "keep", or "right".
-	*/
+  left.fit(data.size());
+  keep.fit(data.size());
+  right.fit(data.size());
 }
 
-string GNB::predict(vector<double>)
+string GNB::predict(vector<double> input)
 {
-	/*
-		Once trained, this method is called and expected to return
-		a predicted behavior for the given observation.
+  double l = left.predict(input);
+  double k = keep.predict(input);
+  double r = right.predict(input);
 
-		INPUTS
-
-		observation - a 4 tuple with s, d, s_dot, d_dot.
-		  - Example: [3.5, 0.1, 8.5, -0.2]
-
-		OUTPUT
-
-		A label representing the best guess of the classifier. Can
-		be one of "left", "keep" or "right".
-		"""
-		# TODO - complete this
-	*/
+  if (l < k && l < r) {
+    return this->possible_labels[0];
+  }
+  if (k < l && k < r) {
+    return this->possible_labels[1];
+  }
+  if (r < k && r < l) {
+    return this->possible_labels[2];
+  }
 
 	return this->possible_labels[1];
 
+}
+
+void GNB_Class::add_data(vector<double> input) {
+  s.add_data(input[0]);
+  d.add_data(input[1]);
+  s_dot.add_data(input[2]);
+  d_dot.add_data(input[3]);
+}
+
+void GNB_Class::fit(int total_count) {
+  s.fit();
+  d.fit();
+  s_dot.fit();
+  d_dot.fit();
+
+  class_probabilty = s.data.size() / (double) total_count;
+}
+
+double GNB_Class::predict(vector<double> input) {
+  double s_prob = s.get_probability(input[0]);
+  double d_prob = d.get_probability(input[1]);
+  double s_dot_prob = s_dot.get_probability(input[2]);
+  double d_dot_prob = d_dot.get_probability(input[3]);
+
+  //cout << "s_prob=" << s_prob << " d_prob=" << d_prob;
+  //cout << " s_dot_prob=" << s_dot_prob << " d_dot_prob=" << d_dot_prob << endl;
+
+  return s_prob *  d_prob * d_prob * d_dot_prob;
+}
+
+void GNB_Feature::add_data(double input) {
+  data.push_back(input);
+}
+
+double GNB_Feature::calc_mean() {
+  double sum = 0;
+  for (int i = 0; i < data.size(); i++) {
+    sum += data[i];
+  }
+  mean = sum/data.size();
+}
+
+double GNB_Feature::calc_variance() {
+  double sum = 0;
+  for (int i = 0; i < data.size(); i++) {
+    double val = (data[i] - mean);
+    sum += val * val;
+  }
+  variance_squared = sum/data.size();
+}
+
+void GNB_Feature::fit() {
+  calc_mean();
+  calc_variance();
+
+  cout << mean << " "  << variance_squared << endl;
+}
+
+double GNB_Feature::get_probability(double x) {
+  if (variance_squared == 0) {
+    return 1;
+  }
+  double power = -(x - mean) * (x - mean) / (2 * variance_squared);
+  double a = sqrt(2 * M_PI * variance_squared);
+  return 1/(a * pow(M_E, power));
 }
--- classifier.h
+++ classifier.h
@@ -8,6 +8,34 @@
 
 using namespace std;
 
+class GNB_Feature {
+public:
+  vector<double> data;
+  double mean;
+  double variance_squared;
+
+  void add_data(double input);
+  double calc_mean();
+  double calc_variance();
+  void fit();
+
+  double get_probability(double x);
+};
+
+class GNB_Class {
+public:
+  GNB_Feature s;
+  GNB_Feature d;
+  GNB_Feature s_dot;
+  GNB_Feature d_dot;
+
+  double class_probabilty;
+
+  void add_data(vector<double> input);
+  void fit(int total_count);
+  double predict(vector<double>);
+};
+
 class GNB {
 public:
 
@@ -26,8 +54,12 @@ public:
 
  	void train(vector<vector<double> > data, vector<string>  labels);
 
-  	string predict(vector<double>);
+  string predict(vector<double>);
 
+private:
+  GNB_Class left;
+  GNB_Class keep;
+  GNB_Class right;
 };
 
 #endif
